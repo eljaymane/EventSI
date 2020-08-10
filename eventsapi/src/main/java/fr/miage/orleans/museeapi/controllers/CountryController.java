ac34fadf172cec6a8390ec75f46f9dcf38ecad6e
Aymane El Jahrani <aymane.el-jahrani@capgemini.com>
2020-08-10 19:36:47 +0200

    Added CRUD methods for country

--- eventsapi/src/main/java/fr/miage/orleans/museeapi/controllers/CountryController.java
+++ eventsapi/src/main/java/fr/miage/orleans/museeapi/controllers/CountryController.java
@@ -1,11 +1,27 @@
 package fr.miage.orleans.museeapi.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.ws.rs.core.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import fr.miage.orleans.modele.Country;
+import fr.miage.orleans.modele.User;
 import fr.miage.orleans.museeapi.dao.CountryRepository;
+import fr.miage.orleans.museeapi.dao.UserRepository;
 
 @RestController
 @CrossOrigin
@@ -14,5 +30,50 @@ public class CountryController {
 
     @Autowired
     private CountryRepository countryRepository;
+
+    //Create
+    @PostMapping("/addCountry")
+    public ResponseEntity<Country> createCountry(@RequestBody Country country){
+        List<Country> existingCountry = countryRepository.findByCountryName(country.getCountryName());
+
+        if(existingCountry.size() > 0) {
+            return new ResponseEntity<Country>((Country)null,HttpStatus.CONFLICT);
+        } else {
+            countryRepository.save(country);
+        }
+
+        return new ResponseEntity<Country>(country,HttpStatus.CREATED);
+    }
+
+    //Read
+    @GetMapping("/country/{id}")
+    public ResponseEntity<Country> getCountry(@PathVariable long id) {
+        if(countryRepository.findById(id) != null){
+            Country country = countryRepository.getOne(id);
+            return new ResponseEntity<Country>(country,HttpStatus.OK);
+        }
+        return new ResponseEntity<Country>((Country)null,HttpStatus.NOT_FOUND);
+          
+    }
+
+    //Update
+    public ResponseEntity<Country> updateCountry(@RequestBody Country country){
+        if(countryRepository.findById(country.getId())!= null){
+            countryRepository.save(country);
+            return new ResponseEntity<Country>(country,HttpStatus.OK);
+        }
+        return new ResponseEntity<Country>((Country)null,HttpStatus.NOT_FOUND);
+    }
+
+    //Delete
+    @DeleteMapping("/country/{id}")
+    public ResponseEntity deleteCountry(@PathVariable long id) {
+        Optional<Country> country = countryRepository.findById(id);
+        if(country != null){
+            countryRepository.delete(country.get());
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
     
 }
--- eventsapi/src/main/java/fr/miage/orleans/museeapi/dao/CountryRepository.java
+++ eventsapi/src/main/java/fr/miage/orleans/museeapi/dao/CountryRepository.java
@@ -1,10 +1,14 @@
 package fr.miage.orleans.museeapi.dao;
 
+import java.util.List;
+
 import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.miage.orleans.modele.Country;
 
 public interface CountryRepository extends JpaRepository<Country,Long> {
+
+    List<Country> findByCountryName(String countryName);
     
 }
