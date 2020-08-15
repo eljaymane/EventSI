14cb6525ec4c8259b993bd3cab1fd6355b251ca9
Aymane El Jahrani <aymane.el-jahrani@capgemini.com>
2020-08-15 13:55:52 +0200

    Api controllers : OK

--- eventsapi/src/main/java/fr/miage/orleans/museeapi/controllers/UserController.java
+++ eventsapi/src/main/java/fr/miage/orleans/museeapi/controllers/UserController.java
@@ -1,10 +1,21 @@
 package fr.miage.orleans.museeapi.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.miage.orleans.modele.User;
 import fr.miage.orleans.museeapi.dao.UserRepository;
 
 @RestController
@@ -13,5 +24,49 @@ import fr.miage.orleans.museeapi.dao.UserRepository;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @PostMapping("/addUser")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        List<User> existingUser = UserRepository.findByUsername(user.getUsername());
+
+        if(existingUser.size() > 0) {
+            return new ResponseEntity<User>((User)null,HttpStatus.CONFLICT);
+        } else {
+            userRepository.save(user);
+        }
+
+        return new ResponseEntity<User>(user,HttpStatus.CREATED);
+    }
+
+    //Read
+    @GetMapping("/User/{id}")
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        if(userRepository.findById(id) != null){
+            User user = userRepository.getOne(id);
+            return new ResponseEntity<User>(user,HttpStatus.OK);
+        }
+        return new ResponseEntity<User>((User)null,HttpStatus.NOT_FOUND);
+          
+    }
+
+    //Update
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        if(userRepository.findById(user.getId())!= null){
+            userRepository.save(user);
+            return new ResponseEntity<User>(user,HttpStatus.OK);
+        }
+        return new ResponseEntity<User>((User)null,HttpStatus.NOT_FOUND);
+    }
+
+    //Delete
+    @DeleteMapping("/User/{id}")
+    public ResponseEntity deleteUser(@PathVariable long id) {
+        Optional<User> User = userRepository.findById(id);
+        if(User.get() != null){
+            userRepository.delete(User.get());
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
     
 }
--- eventsapi/src/main/java/fr/miage/orleans/museeapi/dao/EventRepository.java
+++ eventsapi/src/main/java/fr/miage/orleans/museeapi/dao/EventRepository.java
@@ -1,9 +1,15 @@
 package fr.miage.orleans.museeapi.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.miage.orleans.modele.Event;
 
 public interface EventRepository extends JpaRepository<Event,Long> {
+
+	static List<Event> findByEventName(String eventName) {
+		return null;
+	}
     
 }
